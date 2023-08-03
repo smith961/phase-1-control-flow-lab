c9f572cc3714731296a4fedb2cf73cc02e8d9dc9
unknown <smithbabawande@gmail.com>
2023-08-03 14:37:51 +0100

    completed message

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let ride;
+  if(distance <= 400){
+    //gives customer a free sample
+    ride = "This one is on me!"
+  }
+  else if (distance > 2000 && distance <= 2500){
+    ride = "I will gladly take your thirty bucks."
+  }
+  else {
+    ride = "No can do."
+  }
+
+  return  ride
+}
+scuberGreetingForFeet(199);
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  
+ return  city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+    
+      case "not as generous":
+        return "Thank you."
+        
+        default:
+          return "Bye."
+  }
+
 }
